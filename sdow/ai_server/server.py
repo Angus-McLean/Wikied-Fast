f87efefbbf01158517b0e0d47921db326ae3b612
amclean <amclean@gorillagroup.com>
2019-11-17 12:13:23 -0500

    adds flask to jupyter notebook

--- sdow/ai_server/server.py
+++ sdow/ai_server/server.py
@@ -10,11 +10,12 @@ app = Flask(__name__)
 @app.route('/recommend_page/', methods=['POST'])
 def makecalc():
     data = request.get_json()
+    print(data)
     # prediction = np.array2string(model.predict(data))
 
     return jsonify(data)
 
 if __name__ == '__main__':
-    modelfile = 'models/final_prediction.pickle'
-    # model = p.load(open(modelfile, 'rb'))
+    modelfile = '../../models/clfRF100.sav'
+    model = p.load(open(modelfile, 'rb'))
     app.run(port=5001)
